28f38970c48b70ed411d113633a6898b4c2f799f
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2025-12-09 18:15:12 +0200

    Added structure for unittesting.

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+
+@pytest.fixture
+def sample_config():
+    return {
+        "employees": [
+            {"name": "Служител 1", "start_date": "2025-01-01", "last_shift": None},
+            {"name": "Служител 2", "start_date": "2025-01-01", "last_shift": None},
+            {"name": "Служител 3", "start_date": "2025-01-01", "last_shift": None},
+            {"name": "Служител 4", "start_date": "2025-01-01", "last_shift": None},
+        ],
+        "admin": {
+            "name": "Администратор",
+            "start_date": "2025-01-01"
+        }
+    }
+
+
+@pytest.fixture
+def empty_last_month():
+    return None
+
+
